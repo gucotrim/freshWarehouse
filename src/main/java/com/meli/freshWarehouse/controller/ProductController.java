db49464559586d69d1a5b3cf4a9df08577e52055
Mateus de Oliveira Barbosa <mateus.doliveira@mercadolivre.com>
2022-08-08 13:35:39 -0300

    [Controller - Product] Creating product controller

---
+++ src/main/java/com/meli/freshWarehouse/controller/ProductController.java
@@ -0,0 +1,82 @@
+package com.meli.freshWarehouse.controller;
+
+
+import com.meli.freshWarehouse.dto.ProductDTO;
+import com.meli.freshWarehouse.exception.NotFoundException;
+import com.meli.freshWarehouse.model.Product;
+import com.meli.freshWarehouse.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/fresh-products/product")
+public class ProductController {
+
+    @Autowired
+    private IProductService productService;
+
+    /**
+     * Saves a new product
+     * @return Saves a new product, returns an exception if a product ID isn't found.
+     * @throws NotFoundException When a product doesn't exist.
+     * @see <a href="http://localhost:8080/api/v1/product">Saves a product</a>
+     */
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductDTO newProductDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(newProductDTO));
+    }
+
+    /**
+     * Get all products
+     * @return Returns a list with all saved products.
+     * @see <a href="http://localhost:8080/api/v1/product">Get all products</a>
+     */
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
+    }
+
+    /**
+     * Gets product by id
+     *
+     * @return Product with the designed iD.
+     * @throws NotFoundException When a product doesn't exist with the informed ID.
+     * @see <a href="http://localhost:8080/api/vi/product/{id}">Get the product by id</a>
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProductById(id));
+    }
+
+
+    /**
+     * Updates a product by id
+     *
+     * @return Update the product with the informed id.
+     * @throws NotFoundException When a product with the informed ID isn't found.
+     * @see <a href="http://localhost:8080/api/vi/product">Update a product</a>
+     */
+    @PutMapping
+    public ResponseEntity<Product> update(@RequestBody Product Product) {
+        return new ResponseEntity<>(productService.update(Product), HttpStatus.OK);
+    }
+
+
+    /**
+     * Delete a product by id
+     *
+     * @return Delete the product with the informed id.
+     * @throws NotFoundException When a product with the informed ID isn't found.
+     * @see <a href="http://localhost:8080/api/vi/product">Delete a warehouse</a>
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        productService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
