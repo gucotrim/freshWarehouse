7c1c7c34a27f93a8e394afbdd2abecdafd10125b
Angélica Lyssa <angelica.ccastro@mercadolivre.com>
2022-08-09 13:40:46 -0300

    [Controller-InboundOrder] implementing controller functionality.

--- src/main/java/com/meli/freshWarehouse/controller/InboundOrderController.java
+++ src/main/java/com/meli/freshWarehouse/controller/InboundOrderController.java
@@ -4,6 +4,9 @@ import com.meli.freshWarehouse.dto.InboundOrderDto;
 import com.meli.freshWarehouse.dto.ProductDTO;
 import com.meli.freshWarehouse.exception.NotFoundException;
 import com.meli.freshWarehouse.model.InboundOrder;
+import com.meli.freshWarehouse.repository.InboundOrderRepo;
+import com.meli.freshWarehouse.service.InboundOrderService;
+import com.meli.freshWarehouse.service.RepresentativeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +16,11 @@ import javax.validation.Valid;
 @RequestMapping("/api/v1/fresh-products/inboundorder")
 public class InboundOrderController {
 
+    private final InboundOrderService inboundOrderService;
+    public InboundOrderController(InboundOrderService inboundOrderService) {
+        this.inboundOrderService = inboundOrderService;
 
+    }
     /**
      * Updates a inboundorder by id
      *
@@ -21,6 +28,8 @@ public class InboundOrderController {
      * @throws InboundOrderNotFoundException When a inboundorder with the informed ID isn't found.
      * @see <a href="http://localhost:8080/api/v1/inboundorder">Update a inboundorder</a>
      */
+
+
     @PutMapping("/{id}")
     public ResponseEntity<InboundOrder> update(@PathVariable Long id, @RequestBody @Valid InboundOrderDto inboundOrderDto) {
         return ResponseEntity.ok(inboundOrderService.update(id, inboundOrderDto));
