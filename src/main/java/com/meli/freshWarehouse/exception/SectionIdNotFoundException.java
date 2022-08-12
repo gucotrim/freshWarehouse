a60a6b1679960d0b22c4ff2c35cd38ab22587431
Angélica Lyssa <angelica.ccastro@mercadolivre.com>
2022-08-12 10:52:07 -0300

    [Exceptions] Implementing and fixing Exception handlers.

--- src/main/java/com/meli/freshWarehouse/exception/SectionIdNotFoundException.java
+++ src/main/java/com/meli/freshWarehouse/exception/SectionIdNotFoundException.java
@@ -1,2 +1,14 @@
-package com.meli.freshWarehouse.exception;public class SectionIdNotFoundException {
+package com.meli.freshWarehouse.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+
+public class SectionNotFoundException extends RuntimeException{
+
+    public SectionNotFoundException(String message) {
+        super(message);
+    }
+
 }
